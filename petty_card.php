b413e02a8ebe072afffff3e5ffe424f1d3753f88
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-10-18 17:46:20 +0200

    Update petty_card.php

--- petty_card.php
+++ petty_card.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://flutterwave.com/pay/w3ef0pybjg3x');
+header('Location: https://flutterwave.com/pay/w3ef0pybjg3x?_gl=1%2a15dvi80%2a_ga%2aMTQwNDA0MTU0OS4xNjYwMzg4MDAx%2a_ga_KQ9NSEMFCF%2aMTY2NjEwNzM5NC41Ny4xLjE2NjYxMDc1MTguMzAuMC4w');
 ?>
